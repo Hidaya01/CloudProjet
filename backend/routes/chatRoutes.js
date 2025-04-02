44ae07a5b8f681a6efc5df24af369c476cf56f58
hidaya <hidaya.khalifi@gmail.com>
2025-04-02 10:25:50 +0000

    last-commit-v

--- backend/routes/chatRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-
-router.get('/messages', (req, res) => {
-    res.send('List of chat messages will be here');
-});
-
-module.exports = router;
--- backend/services/notificationService.js
+++
@@ -1,5 +0,0 @@
-function sendNotification(io, message) {
-    io.emit('notification', message);
-}
-
-module.exports = { sendNotification };
